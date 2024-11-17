72f2fdbfb3c7a7f2ef91e4a98833cd2291257ad0
khushburajak <khushburajak17@gmail.com>
2024-11-17 17:53:08 +0545

    factorial of 8

---
+++ factorial.java
@@ -0,0 +1,13 @@
+public class factorial{
+    public static void main(String[] args) {
+       int factorial=1;
+       int a=8;
+       for(int i=1;i<=a;i++){
+        factorial*=i;       
+       }
+       System.out.println("The factorial of " +a +" is "+factorial);
+       
+
+    }
+
+}
