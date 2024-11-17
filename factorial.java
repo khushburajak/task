1ebb644b0e342c2eaa6059d3cdec2de6d0abaa3f
khushburajak <khushburajak17@gmail.com>
2024-11-17 22:10:46 +0545

    factorial

--- factorial.java
+++ factorial.java
@@ -1,13 +1,28 @@
-public class factorial{
-    public static void main(String[] args) {
-       int factorial=1;
-       int a=8;
-       for(int i=1;i<=a;i++){
-        factorial*=i;       
-       }
-       System.out.println("The factorial of " +a +" is "+factorial);
+// public class factorial{
+//     public static void main(String[] args) {
+//        int factorial=1;
+//        int a=8;
+//        for(int i=1;i<=a;i++){
+//         factorial*=i;       
+//        }
+//        System.out.println("The factorial of " +a +" is "+factorial);
        
 
-    }
+//     }
 
+// }
+
+public class factorial{
+    int facto=1;
+    int fact(int a){
+        for(int i=1;i<=a;i++)
+        facto*=i;
+        return facto; 
+    }
+    public static void main(String[] args) {
+        factorial fact=new factorial();
+        int facto=fact.fact(5);
+        System.out.println(facto);
+    }
 }
+
